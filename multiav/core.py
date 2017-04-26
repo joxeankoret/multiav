48d600772555c834c40d66155a670c1422ca65f4
xiao <xiao-yao-tn@ynu.jp>
2017-04-26 16:33:54 +0900

    fix a bug

--- multiav/core.py
+++ multiav/core.py
@@ -445,7 +445,7 @@ class CAvgScanner(CAvScanner):
     # Considered fast because it requires the daemon to be running.
     # This is why...
     self.speed = AV_SPEED_ULTRA
-    self.pattern = "\>{0,1}(.*) \s+[a-z]+\s+[a-z]+\s+(.*)"
+    self.pattern1 = "\>{0,1}(.*) \s+[a-z]+\s+[a-z]+\s+(.*)"
     self.pattern2 = "\>{0,1}(.*) \s+[a-z]+\s+(.*)" #like this:Luhe.Fiha.A
 
   def scan(self, path):
